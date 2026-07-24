106cd4ae0c4e386d4479c1c63e3993df7f1103f2
HaiAnh <62981228+misuTAMC@users.noreply.github.com>
2026-07-24 23:02:25 +0700

    secmantic_search_start

---
+++ cli/lib/semantic_search.py
@@ -0,0 +1,14 @@
+from sentence_transformers import SentenceTransformer
+class SemanticSearch:
+    def __init__(self):
+        self.model=SentenceTransformer("all-MiniLM-L6-v2")
+    
+    
+    
+    
+    
+def verify_model():
+    search_instance=SemanticSearch()
+    
+    print(f"Model loaded : {search_instance.model}")
+    print(f"Max sequence length : {search_instance.model.max_seq_length}")
